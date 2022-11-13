3f087486164bc96cca67a05243e1fc47699cb8db
Танцюра Екатерина <tantsiura.e@yandex.ru>
2022-11-13 23:39:11 +0300

    Спринт 3: Сдача проекта, попытка №1

---
+++ yatube/yatube/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path 
+
+urlpatterns = [
+    path('', include('posts.urls', namespace='index')),
+    path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
+    path('admin/', admin.site.urls),
+]
