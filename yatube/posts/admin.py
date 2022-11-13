6cda646483adf05cf4a60b05759b21b67b99867c
Танцюра Екатерина <tantsiura.e@yandex.ru>
2022-11-13 23:54:19 +0300

    Спринт 3: Сдача проекта, внесение правок

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Group, Post
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-# Главная страница
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
@@ -10,13 +10,13 @@ def index(request):
     }
     return render(request, template, context)
 
-# Страница с постами,отфильтрованными по группам
+
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    #template = 'posts/group_list.html'
+    template = 'posts/group_list.html'
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context) 
+    return render(request, template, context)
