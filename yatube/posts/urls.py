29d46914d56c77ace926bc5b59659d1234eb5faa
Танцюра Екатерина <tantsiura.e@yandex.ru>
2022-11-14 12:18:39 +0300

    Внесение правок PEP8

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 app_name = 'posts'
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list'),
-]
-
-# проверка
+urlpatterns = [path('', views.index, name='index'),
+               path('group/<slug:slug>/', views.group_posts, name='group_list'),]
