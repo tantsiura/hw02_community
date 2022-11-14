0018cf9bb28103c89bd496f844682978d577fc74
Танцюра Екатерина <tantsiura.e@yandex.ru>
2022-11-14 13:16:53 +0300

    pep8

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [path('', views.index, name='index'),
-               path('group/<slug:slug>/', 
+               path('group/<slug:slug>/',
                views.group_posts, 
                name='group_list'), ]
+
