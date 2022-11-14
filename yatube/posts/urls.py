9e67905c436994e857ba925a557a6896d02494e0
Танцюра Екатерина <tantsiura.e@yandex.ru>
2022-11-14 13:44:00 +0300

    final commit

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
+
+app_name = 'posts'
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='index')),
-    path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
-    path('admin/', admin.site.urls), ]
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'), ]
